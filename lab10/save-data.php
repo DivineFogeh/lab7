4c3d33d2388de9b012ef8a98e3b528fc527deb4e
DivineFogeh <113813936+DivineFogeh@users.noreply.github.com>
2023-11-05 23:28:59 -0600

    Update save-data.php

--- lab10/save-data.php
+++ lab10/save-data.php
@@ -1,24 +1,16 @@
 <?php
-require 'vendor/autoload.php'; // Include the Azure SDK for PHP
-
-use Azure\Storage\Blob\BlobRestProxy;
-
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=webtechstorage1;AccountKey=EUluiINM7OGl3WssBRpho7cNpZSzDjN0n5/ew7Le44YBhiieO0LFPqeA4iv1VzcPpWj2nfbikAn8+AStMfcfkg==";
-
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-
-$containerName = 'webtechcontainer';
-$blobName = 'data.txt';
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
-    $newData = $first_name . ' ' . $last_name;
 
-    // Save data to Azure Blob Storage
-    $options = new CreateBlockBlobOptions();
-    $options->setContentType('text/plain');
-    $blobClient->createBlockBlob($containerName, $blobName, $newData, $options);
+    // Data to append to the file
+    $data = $first_name . ' ' . $last_name . "\n";
+
+    // Specify the path to the non-public folder
+    $file_path = '/path/to/private-folder/data.txt';
+
+    // Append data to the file
+    file_put_contents($file_path, $data, FILE_APPEND);
 
     echo "Data saved successfully.";
 } else {
