61e24fdeb38d82fc500b52a807a1fc102375a9a2
DivineFogeh <113813936+DivineFogeh@users.noreply.github.com>
2023-11-05 23:42:51 -0600

    Update save-data.php

--- lab10/save-data.php
+++ lab10/save-data.php
@@ -1,16 +1,53 @@
 <?php
+require 'vendor/autoload.php'; // Include the Azure SDK for PHP
+
+use Azure\Storage\Blob\BlobRestProxy;
+use Azure\Storage\Blob\Models\CreateBlockBlobOptions;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=webtechstorage1;AccountKey=<?php
+require 'vendor/autoload.php'; // Include the Azure SDK for PHP
+
+use Azure\Storage\Blob\BlobRestProxy;
+use Azure\Storage\Blob\Models\CreateBlockBlobOptions;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=webtechstorage1;AccountKey=YOUR_ACCOUNT_KEY";
+
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
+$containerName = 'webtechcontainer';
+$blobName = 'data.txt';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
+    $newData = $first_name . ' ' . $last_name;
 
-    // Data to append to the file
-    $data = $first_name . ' ' . $last_name . "\n";
+    // Save data to Azure Blob Storage
+    $options = new CreateBlockBlobOptions();
+    $options->setContentType('text/plain');
+    $blobClient->createBlockBlob($containerName, $blobName, $newData, $options);
 
-    // Specify the path to the non-public folder
-    $file_path = '/path/to/private-folder/data.txt';
+    echo "Data saved successfully.";
+} else {
+    echo "Invalid request.";
+}
+?>
+";
 
-    // Append data to the file
-    file_put_contents($file_path, $data, FILE_APPEND);
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
+$containerName = 'webtechcontainer';
+$blobName = 'data.txt';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $newData = $first_name . ' ' . $last_name;
+
+    // Save data to Azure Blob Storage
+    $options = new CreateBlockBlobOptions();
+    $options->setContentType('text/plain');
+    $blobClient->createBlockBlob($containerName, $blobName, $newData, $options);
 
     echo "Data saved successfully.";
 } else {
