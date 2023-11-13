8422f2fcd6155ce157d81015d8182c0c3c7b71f9
DivineFogeh <113813936+DivineFogeh@users.noreply.github.com>
2023-11-12 19:30:47 -0600

    Update cookie.php

--- lab12/cookie.php
+++ lab12/cookie.php
@@ -1,7 +1,7 @@
 <?php
 // Define the cookie name and value
 $cookie_name = "cs4413";
-$cookie_value = "your_value_here"; // You can choose any value for the cookie
+$cookie_value = "2"; // You can choose any value for the cookie
 
 // Define the expiration time (1 week from now in this example)
 $expiration_time = time() + (7 * 24 * 60 * 60); // 7 days * 24 hours * 60 minutes * 60 seconds
