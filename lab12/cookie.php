098804f5db481ef0d0286b313bdb526d0176e3f7
DivineFogeh <113813936+DivineFogeh@users.noreply.github.com>
2023-11-12 19:33:05 -0600

    Update cookie.php

--- lab12/cookie.php
+++ lab12/cookie.php
@@ -3,8 +3,8 @@
 $cookie_name = "cs4413";
 $cookie_value = "2"; // You can choose any value for the cookie
 
-// Define the expiration time (1 week from now in this example)
-$expiration_time = time() + (7 * 24 * 60 * 60); // 7 days * 24 hours * 60 minutes * 60 seconds
+// Define the expiration time (2 minutes from now)
+$expiration_time = time() + (2 * 60); // 2 minutes * 60 seconds
 
 // Check if the cookie is already set
 if(isset($_COOKIE[$cookie_name])) {
@@ -13,6 +13,7 @@ if(isset($_COOKIE[$cookie_name])) {
     // Set the cookie
     setcookie($cookie_name, $cookie_value, $expiration_time, "/");
 
-    echo "Cookie named '" . $cookie_name . "' is now set with value: " . $cookie_value;
+    // Output the correct message
+    echo "Cookie named '" . $cookie_name . "' is now set with value: " . $cookie_value . " and will expire in 2 minutes";
 }
 ?>
